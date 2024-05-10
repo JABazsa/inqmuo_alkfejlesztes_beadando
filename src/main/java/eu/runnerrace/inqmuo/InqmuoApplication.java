5c101b8c9141b39761d11be2a147a81b7aab7917
JABazsa <adrian.b.janosik@gmail.com>
2024-05-10 21:39:58 +0200

    round2

---
+++ src/main/java/eu/runnerrace/inqmuo/InqmuoApplication.java
@@ -0,0 +1,13 @@
+package eu.runnerrace.inqmuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqmuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InqmuoApplication.class, args);
+	}
+
+}
